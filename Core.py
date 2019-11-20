af22688ced297426b1e7d9bf13e98d8a6fa667bf
mtrj <57919946+mtrj@users.noreply.github.com>
2019-11-20 16:23:52 -0300

    Update Core.py

--- Core.py
+++ Core.py
@@ -1,7 +1,63 @@
 import numpy as np
 import scipy.stats.stats as sss
 import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+import requests as requests
 
+# Exemplo para rodar o Markowitz usando dados de 1y diários do yahoo:
+# Mark = Markowitz(BaixaAcao(['PETR4','VALE3','BRKM5','VVAR3','TIMP3','ABEV3']).matrizretornos(),np.random.rand(6))
+# Neste caso baixamos 6 ações e as transformamos em um objeto da classe de Markowitz para que seja utilizado para qualquer fim,
+# os pesos são determinados por uma lista aleatória do numpy
+# Caso a simulação seja de fronteira eficiente:
+# Mark.PortfolioAleatorio(100000)
+# Onde serão gerados 100.000 portfólios aleatórios e plotados, caso o usuário queira salvar o arquivo com os resultados e o gráfico
+# deve mudar o caminho destes no código do PortfolioAleatorio.
+
+def left(s, amount):
+    return s[:amount]
+def right(s, amount):
+    return s[-amount:]
+def mid(s, offset, amount):
+    return s[offset:offset+amount]
+    
+class BaixaAcao:
+    def __init__(self, tickerarr):
+        self.tickerarr = tickerarr
+    
+    def download(self):
+        tickerarr = self.tickerarr
+        precos = []
+        for x in range(len(tickerarr)):
+            headers = {"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+            page = requests.get('https://query1.finance.yahoo.com/v8/finance/chart/' + tickerarr[x] + '.SA?region=BR&lang=pt-BR&includePrePost=false&interval=1d&range=1y&corsDomain=br.financas.yahoo.com&.tsrc=finance', headers=headers)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            adj = str(soup).split()
+            adj = str(soup).split('{')
+            adj2 = str(adj).split(',')
+            adjclosearr =[]
+            #Quebras do arquivo:
+            for i in range(1536,1784,1):
+                if i==1536:
+                    numero = float(adj2[1536].replace(left(adj2[1536],14),"")*1)
+                    adjclosearr.append(numero)
+                elif i!=1536 and i!=1783:
+                    numero = float(adj2[i]*1)
+                    adjclosearr.append(numero)
+                elif i==1783 or i==1784:
+                    numero = float(adj2[i].replace(right(adj2[i],6),"")*1)
+                    adjclosearr.append(numero)
+            precos.append(adjclosearr)
+        if len(tickerarr)==1:
+            return(adjclosearr)
+        elif len(tickerarr)>1:
+            return(precos)
+    
+    def matrizretornos(self):
+        tickerarr = self.tickerarr
+        matrizprecos = BaixaAcao(tickerarr).download()
+        RA = [Precos(matrizprecos[i]).Retornos() for i in range(len(tickerarr))]
+        return(RA)
+        
 class Precos:
     def __init__(self,Precos):
         self.Precos = Precos
@@ -113,7 +169,7 @@ class Markowitz:
         plt.style.use('seaborn-whitegrid')
         plt.xlabel('Volatilidade')
         plt.ylabel('Retorno')
-        grafico = plt.scatter(VolArr, RetArr, c=SharpeArr, cmap='viridis') #ou trocar cmap='inferno'
+        grafico = plt.scatter(VolArr, RetArr, c=SharpeArr, cmap='inferno')
         pontoverm = plt.scatter(VolSM, RetSM,c='red', s=50)
         plt.colorbar(grafico, label='Sharpe')
         plt.savefig("C:/Users/milto/Desktop/MarkowitzTeste.png")
