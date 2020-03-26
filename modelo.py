9cadcead3d572cb398d9007b24d0d53b96860d5d
mtrj <57919946+mtrj@users.noreply.github.com>
2020-03-26 20:33:45 -0300

    Update modelo.py

--- modelo.py
+++ modelo.py
@@ -4,9 +4,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import date, timedelta
+from datetime import datetime as dt
 import yfinance as yf
 import time
-
+from lib_yahoo import yahoo
+inicio = date(2019,5,22)
+#inicio_timestamp = int(dt.timestamp(dt(2019,6,26)))
 
 class StockData:
     """ Classe responsável por download e manipulação dos dados do yfinance """
@@ -20,7 +23,7 @@ class StockData:
     def _download_data(self):
         for ticker in self.tickers:
             yf_tk = ticker + '.sa'
-            self._complete_data[ticker] = yf.download(tickers=yf_tk,start=date(2010,1,1),end=self.end_date,progress=False)
+            self._complete_data[ticker] = yf.download(tickers=yf_tk,start=inicio,end=self.end_date,progress=False)
         
     def get_ticker_data(self, ticker):
         """Retorna as todos os dados para um ticker"""
@@ -113,14 +116,44 @@ class Markowitz:
         sharpe.sort()
         if sd==False: return [self.tickers[sp_backup.index(sharpe[-i])] for i in range(n)]
         if sd==True: return sharpe
+    
+    def _worst_sharpe(self, n=5,sd=False):
+        """Retorna o número n de ativos de maior sharpe na lista tickers"""
+        sharpe = [(self._avg_rets[i]-self.dio)/self._avg_vol[i] for i in range(len(self.tickers))]
+        sp_backup = list(sharpe)
+        sharpe.sort()
+        if sd==False: return [self.tickers[sp_backup.index(sharpe[i])] for i in range(n)]
+        if sd==True: return sharpe
         
-def backtest_markowitz(tickers, ndays, plot=False):
+    def _stats(self, plot=[], inicio_plot=50):
+        """
+        Função para plotar as estatísticas dos dados de ações
+        """
+        df_out = pd.DataFrame(index=self.quotes.index)
+        for i in range(len(self.tickers)):
+            ticker=self.tickers[i]
+            rets_hist = [np.log(self.quotes[:i][ticker]/self.quotes[:i][ticker].shift(1)).mean()*252 for i in range(len(self.quotes))]
+            vols_hist = [np.log(self.quotes[:i][ticker]/self.quotes[:i][ticker].shift(1)).std()*np.sqrt(252)
+                         for i in range(len(self.quotes))]
+            sps_hist = [(rets_hist[i]-self.dio)/vols_hist[i] for i in range(len(self.quotes))]
+            df_out[ticker+'_ret'] = rets_hist
+            df_out[ticker+'_vol'] = vols_hist
+            df_out[ticker+'_sharpe'] = sps_hist
+        df_out[[self.tickers[i]+'_vol' for i in range(len(self.tickers))]][inicio_plot:].plot()
+        
+def backtest_markowitz(tickers, ndays, plot=False, ibov_plot=False, override=True, position='long'):
+    """
+    Gera a carteira com o melhor sharpe a cada ndays e computa qual a porcentagem
+    deve ser alocada no ativo, posteriormente faz o PnL baseado nisso
+    """
     #Ainda não tem troca de ativos no meio
-    data = StockData(tickers)
-    dataframe = data.get_column_data('Adj Close').dropna()
-    dataframe_open = data.get_column_data('Open').dropna()
+    if override==False:
+        data = StockData(tickers)
+        dataframe = data.get_column_data('Adj Close').dropna()
+    else:
+        dataframe = yahoo(tickers)._consolidate_dfs()
+        dataframe = dataframe[dataframe.index.get_loc(inicio):]
     df=dataframe[ndays:]
-    #df_open = dataframe_open[ndays:]
     start_time = time.time()
     pesos_aleat = []
     df_sliced = df.iloc[::ndays, :].dropna()
@@ -129,23 +162,22 @@ def backtest_markowitz(tickers, ndays, plot=False):
     dr_list = dataframe.index.to_list()
     for i in range(len(ret_close)):
         close = dataframe[:dr_list.index(dt_recalc[i])]
-        #close = StockData(tickers,end_date=dt_recalc[i]).get_column_data('Adj Close') demora 28x mais tempo
         mark = Markowitz(close)
         pesos_aleat.append(mark.fit(sg=False))
     pesos_aleat = pd.DataFrame(pesos_aleat)
-    avg_close = np.array(pesos_aleat)*np.array(ret_close)
+    if position.lower()=='long': avg_close = np.array(pesos_aleat)*np.array(ret_close)
+    if position.lower()=='short': avg_close = -np.array(pesos_aleat)*np.array(ret_close)
     pnl = pd.DataFrame(np.cumsum([np.sum(avg_close[i]) 
                                   for i in range(len(avg_close))])+1,
-                                  index=dt_recalc[:-1], columns={'Pnl'})
+                                  index=dt_recalc[1:], columns={'Pnl'})
     if plot==True: pnl.plot(color='black')
+    if ibov_plot==True:
+        ibov = yf.download('^BVSP',start=inicio,progress=False)['Adj Close'].dropna()
+        ibov['ret'] = np.log(ibov/ibov.shift(1))
+        ibov['IBOVESPA'] = ibov['ret'].cumsum()+1
+        ibov['IBOVESPA'].plot(color='blue')
     elapsed_time = time.time() - start_time
     print('Tempo de execução: ' + str(round(elapsed_time,4)) + 's')
     return pnl
-        
+            
 tickers= ['abev3','vvar3','jbss3','petr4', 'btow3', 'itub4']
-
-dados = StockData(tickers)
-
-close = dados.get_column_data('Adj Close')
-
-mark = Markowitz(close)
